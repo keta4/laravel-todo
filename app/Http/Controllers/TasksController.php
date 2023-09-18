866dc3531b220f6c4246743632e65158fbe6daa4
keta4 <82580781+keta4@users.noreply.github.com>
2023-09-18 16:17:40 +0900

    タスク一覧画面作成

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Task;
 
 class TasksController extends Controller
 {
     //
+    public function index(){
+        $tasks = Task::get();
+        return view('tasks.index', compact('tasks'));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TasksController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [TasksController::class, 'index'])->name('tasks.index');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
