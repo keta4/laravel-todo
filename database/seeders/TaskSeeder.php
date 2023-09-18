120af7e5fd13be411b51103884b2c48bf35b4d24
keta4 <82580781+keta4@users.noreply.github.com>
2023-09-18 15:14:43 +0900

    TasksテーブルにSeederでテストデータ追加

---
+++ database/seeders/TaskSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class TaskSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('tasks')->insert([
+            ['id' => 1, 'name' => 'task1', 'content' => 'content1'],
+            ['id' => 2, 'name' => 'task2', 'content' => 'content2'],
+            ['id' => 3, 'name' => 'task3', 'content' => 'content3']
+        ]);
+    }
+}
